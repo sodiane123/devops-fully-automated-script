37095c242dfe09b942bcb51dcba05a9495d68c60
sodiane123 <142374578+sodiane123@users.noreply.github.com>
2023-10-11 18:47:00 -0500

    Update deployment-servers-setup.sh

--- deployment-servers-setup.sh
+++ deployment-servers-setup.sh
@@ -24,8 +24,8 @@ sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin/node_export
 rm -rf node_exporter-1.0.1.linux-amd64.tar.gz node_exporter-1.0.1.linux-amd64
 
 # setup the node-exporter dependencies
-sudo yum install git -y
-sudo git clone -b installations https://github.com/abdelazziz20/devops-fully-automated.git /tmp/devops-fully-automated
+sudo yum install git -ys
+sudo git clone -b main https://github.com/abdelazziz20/devops-fully-automated.git /tmp/devops-fully-automated
 sudo cp /tmp/devops-fully-automated/prometheus-setup-dependencies/node-exporter.service /etc/systemd/system/node-exporter.service
 
 sudo systemctl daemon-reload
